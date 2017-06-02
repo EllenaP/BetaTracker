a75558d9ded556e3cf6cb2ffbe48eed5198aa97f
Andy Roberts <djandyr@gmail.com>
2017-06-02 13:45:31 +0100

    Allow navigation of tab from side menu

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,15 +3,22 @@ import {TrackingHomePage} from "../tracking-home/tracking-home";
 import {SupplyChainManagementPage} from "../supply-chain-management/supply-chain-management";
 import {ContactPage} from "../contact/contact";
 import {JsonTestPage} from "../json-test/json-test";
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
-  trackingHomePage = TrackingHomePage;
-  supplyChainManagementPage = SupplyChainManagementPage;
-  contactPage = ContactPage;
-  jsonTestPage = JsonTestPage;
+  trackingHomePage: any  = TrackingHomePage;
+  supplyChainManagementPage: any  = SupplyChainManagementPage;
+  contactPage: any  = ContactPage;
+  jsonTestPage: any  = JsonTestPage;
+  myIndex: any = 0;
+
+  constructor(navParams: NavParams) {
+    // set the default index to 0, the first tab
+    this.myIndex = 0;
+    if (navParams.data.index) this.myIndex = navParams.data.index;
+  }
 
 }
